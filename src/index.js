ba6b62ff5551e3701d2228af5ebc67ddd5fe8e8c
ko7ko <NLs6houh@protonmail.com>
2022-09-12 22:57:32 +0400

    add delete endpoints

--- src/index.js
+++ src/index.js
@@ -64,6 +64,20 @@ app.post('/users', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async(req, res) => {
+    const _id = req.params.id
+    try {
+        const user = await User.findByIdAndDelete(_id)
+
+        if(!user) {
+            return res.status(404).send()
+        }
+        return res.send(user)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
 app.get('/tasks', async (req, res) => {
     try {
         const tasks = await Task.find({})
@@ -117,6 +131,20 @@ app.post('/tasks', async (req, res) => {
         res.status(201).send(task)
     } catch (e) {
         res.status(400).send(e)
+    }
+})
+
+app.delete('/tasks/:id', async(req, res) => {
+    const _id = req.params.id
+    try {
+        const task = await Task.findByIdAndDelete(_id)
+
+        if(!task) {
+            return res.status(404).send()
+        }
+        return res.send(task )
+    } catch (e) {
+        res.status(500).send(e)
     }
 })
 
