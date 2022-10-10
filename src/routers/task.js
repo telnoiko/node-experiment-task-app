da99a8cc33869e21c34aef0ac13b6051f9e7896b
ko7ko <NLs6houh@protonmail.com>
2022-10-11 03:11:37 +0400

    feat: enable tasks sorting

--- src/routers/task.js
+++ src/routers/task.js
@@ -5,11 +5,18 @@ const Task = require('../models/task')
 
 // GET /tasks?completed=true
 // GET /tasks?limit=10&skip=10
+// GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
+    const sort = {}
 
     if(req.query.completed) {
         match.completed = req.query.completed === 'true' 
+    }
+
+    if(req.query.sortBy) {
+        const parts = req.query.sortBy.split(":")
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
 
     try {
@@ -19,7 +26,8 @@ router.get('/tasks', auth, async (req, res) => {
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                limit: parseInt(req.query.skip)
+                limit: parseInt(req.query.skip),
+                sort 
             }
         })
 
